dc52988c0c35086c2b6b1bf1daf1a85e272c4501
lin <lin@qq.com>
2024-06-24 20:32:19 +0800

    微服务01

---
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,17 @@
+package com.hmall.cart;
+
+
+import com.config.DefaultFeignConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+@EnableFeignClients(basePackages ="com.hmall.api.client",defaultConfiguration = DefaultFeignConfig.class)
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+}
